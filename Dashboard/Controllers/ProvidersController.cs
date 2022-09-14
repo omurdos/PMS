4a5cae5b105fdf3401b43c61d8a6ed008d9347b0
Omar Murdos <46492436+omurdos@users.noreply.github.com>
2022-09-14 11:18:24 +0400

    Providers index page design

---
+++ Dashboard/Controllers/ProvidersController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Core;
+using Core.Entities;
+
+namespace Dashboard.Controllers
+{
+    public class ProvidersController : Controller
+    {
+        private readonly DBContext _context;
+
+        public ProvidersController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Providers
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Providers";
+              return _context.Providers != null ? 
+                          View(await _context.Providers.ToListAsync()) :
+                          Problem("Entity set 'DBContext.Providers'  is null.");
+        }
+
+        // GET: Providers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+
+            return View(provider);
+        }
+
+        // GET: Providers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Providers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,ContactName,ContactPhoneNumber,Id,IsDeleted,IsActive,CreatedBy,CreatedAt,ModifiedBy,ModifiedAt")] Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(provider);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(provider);
+        }
+
+        // GET: Providers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers.FindAsync(id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+            return View(provider);
+        }
+
+        // POST: Providers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Name,ContactName,ContactPhoneNumber,Id,IsDeleted,IsActive,CreatedBy,CreatedAt,ModifiedBy,ModifiedAt")] Provider provider)
+        {
+            if (id != provider.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(provider);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProviderExists(provider.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(provider);
+        }
+
+        // GET: Providers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+
+            return View(provider);
+        }
+
+        // POST: Providers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Providers == null)
+            {
+                return Problem("Entity set 'DBContext.Providers'  is null.");
+            }
+            var provider = await _context.Providers.FindAsync(id);
+            if (provider != null)
+            {
+                _context.Providers.Remove(provider);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProviderExists(string id)
+        {
+          return (_context.Providers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,10 +1,12 @@
+using Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DBContext>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
